bdd359e2761610be989a994f9909e0c9f4592d8c
George Du <gdu@mit.edu>
2016-11-19 16:21:22 -0500

    add file reading pipeline

--- model.py
+++ model.py
@@ -1,10 +1,29 @@
 import tensorflow as tf
 import numpy as np
+import glob
 import argparse
 
 class Loader(object):
-    def __init__(self):
-        pass
+    def __init__(self, high_res_info):
+        f2, self.h2, self.w2 = high_res_info
+        self.q2 = tf.train.string_input_producer(f2)
+        self.batch_size = 50
+
+    def _get_pipeline(self, q, h, w):
+        reader = tf.WholeFileReader()
+        key, value = reader.read(q)
+        raw_img = tf.image.decode_png(value, channels=3)
+        my_img = tf.image.per_image_whitening(raw_img)
+        my_img.set_shape((h, w, 3))
+        min_after_dequeue = 1000
+        capacity = min_after_dequeue + 3 * self.batch_size
+        batch = tf.train.shuffle_batch([my_img], batch_size=batch_size, capacity=capacity,
+                min_after_dequeue=min_after_dequeue)
+        small_batch = tf.images.resize_bicubic(batch, [96, 96])
+        return small_batch, batch
+
+    def batch():
+        return self._get_pipeline(self.q2, self.h2, self.w2)
 
 class GAN(object):
     def __init__(self):
@@ -40,10 +59,8 @@ class GAN(object):
 class SuperRes(object):
     def __init__(self, sess, loader):
         self.sess = sess
-        self.loader = loader
-        self.setup_model()
+        self.batch = loader.batch()
 
-    def setup_model(self):
         self.GAN = GAN()
         d_optim = tf.train.AdamOptimizer(self.learning_rate).minimize(self.GAN.d_loss)
         g_optim = tf.train.AdamOptimizer(self.learning_rate).minimize(self.GAN.g_loss)
@@ -53,18 +70,19 @@ class SuperRes(object):
 
         # Train
         for epoch in range(num_epochs):
-            for idx, batch in self.loader.get_next_train_batch():
-                batch_xs, batch_ys = self.loader.get_next_train_batch()
-                self.sess.run(self.train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-                correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+            batch_xs, batch_ys = sess.run(self.batch)
+            self.sess.run(self.d_optim, feed_dict={y: batch_ys})
+            self.sess.run(self.g_optim, feed_dict={x: batch_xs})
+            self.sess.run(self.g_optim, feed_dict={x: batch_xs})
 
     def test_model(self):
         pass
 
 def main():
     sess = tf.Session()
-    loader = Loader()
+    file_list_2 = glob.glob("/etc/gan_images/high_res/*.png")
+    file_info_2 = (file_list_2, 768, 768)
+    loader = Loader(file_info_2)
     model = SuperRes(sess, loader)
     model.train_model()
 
