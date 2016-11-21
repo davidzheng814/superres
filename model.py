f6c4fbb101623a5d498df1ff786b900294990147
David Zheng <dzd123@mit.edu>
2016-11-20 21:36:36 -0500

    Fixed different errors in model.

--- model.py
+++ model.py
@@ -1,117 +1,150 @@
 import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.training import moving_averages
 import numpy as np
 import glob
 import argparse
 
+# TODO SummaryWriter and other important utilities.
+
+IMAGES = "/etc/gan_images/high_res/*.png"
+LOGS_DIR = "/var/log/superres"
+
+HR_HEIGHT = 384
+HR_WIDTH = 384
+r = 4
+LR_HEIGHT = HR_HEIGHT / r
+LR_WIDTH = HR_WIDTH / r
+NUM_CHANNELS = 3
+BATCH_SIZE = 50
+NUM_EPOCHS = 10
+
+LEARNING_RATE = 1e-4
 BN_EPSILON = 0.001
+MOVING_AVERAGE_DECAY = 0.9997
+BN_DECAY = MOVING_AVERAGE_DECAY
+UPDATE_OPS_COLLECTION = 'update_ops'
+BETA_1 = 0.9
 
 class Loader(object):
     def __init__(self, high_res_info):
         f2, self.h2, self.w2 = high_res_info
         self.q2 = tf.train.string_input_producer(f2)
-        self.batch_size = 50
 
     def _get_pipeline(self, q, h, w):
         reader = tf.WholeFileReader()
         key, value = reader.read(q)
-        raw_img = tf.image.decode_png(value, channels=3)
+        raw_img = tf.image.decode_png(value, channels=NUM_CHANNELS)
         my_img = tf.image.per_image_whitening(raw_img)
-        my_img.set_shape((h, w, 3))
+        my_img.set_shape((h, w, NUM_CHANNELS))
         min_after_dequeue = 1000
-        capacity = min_after_dequeue + 3 * self.batch_size
-        batch = tf.train.shuffle_batch([my_img], batch_size=batch_size, capacity=capacity,
+        capacity = min_after_dequeue + 3 * BATCH_SIZE
+        batch = tf.train.shuffle_batch([my_img], batch_size=BATCH_SIZE, capacity=capacity,
                 min_after_dequeue=min_after_dequeue)
-        small_batch = tf.images.resize_bicubic(batch, [96, 96])
+        small_batch = tf.images.resize_bicubic(batch, [LR_HEIGHT, LR_WIDTH])
         return small_batch, batch
 
     def batch():
         return self._get_pipeline(self.q2, self.h2, self.w2)
 
-def relu(x, alpha=0., max_value=None):
+def relu_block(x, alpha=0., max_value=None):
     negative_part = tf.nn.relu(-x)
     x = tf.nn.relu(x)
     if max_value is not None:
-        x = tf.clip_by_value(x, tf.cast(0., dtype=_FLOATX),
-                             tf.cast(max_value, dtype=_FLOATX))
-    x -= tf.constant(alpha, dtype=_FLOATX) * negative_part
+        x = tf.clip_by_value(x, tf.cast(0., dtype=tf.float32),
+                             tf.cast(max_value, dtype=tf.float32))
+    x -= tf.constant(alpha, dtype=tf.float32) * negative_part
     return x
 
-def res_block(inp):
+def res_block(inp, is_training_cond):
     """ResNet B Block.
     TODO: decide whether bias is worth it
-    TODO: Figure out whether moving_mean and moving_variance work.
     """
-    kernel_shape = (1,)
-    bias_shape = (1,)
-    params_shape = (1,)
+    inp_shape = inp.get_shape()
+    assert inp_shape[-1] == 64 # Must have 64 channels to start.
 
-    weights = tf.get_variable('weights1', kernel_shape,
-        initializer=tf.random_normal_initializer())
-    beta = tf.get_variable('beta1', params_shape, initializer=tf.zeros_initializer)
-    gamma = tf.get_variable('gamma1', params_shape, initializer=tf.ones_initializer)
-    moving_mean = tf.get_variable('moving_mean1', params_shape, initializer=tf.zeros_initializer)
-    moving_variance = tf.get_variable('moving_variance1', params_shape, initializer=tf.ones_initializer)
+    with tf.variable_scope("resconv1"):
+        h = conv_block(inp, relu=True, bn=True, is_training_cond=is_training_cond)
 
-    h = tf.nn.conv2d(inp, weights, padding='SAME', strides=[1, 1, 1, 1])
-    h = tf.nn.batch_normalization(h, moving_mean, moving_variance, beta, gamma, BN_EPSILON)
-    h = tf.nn.relu(h)
-
-    weights = tf.get_variable('weights2', kernel_shape,
-        initializer=tf.random_normal_initializer())
-    beta = tf.get_variable('beta2', params_shape, initializer=tf.zeros_initializer)
-    gamma = tf.get_variable('gamma2', params_shape, initializer=tf.ones_initializer)
-    moving_mean = tf.get_variable('moving_mean2', params_shape, initializer=tf.zeros_initializer)
-    moving_variance = tf.get_variable('moving_variance2', params_shape, initializer=tf.ones_initializer)
-
-    h = tf.nn.conv2d(inp, weights, padding='SAME', strides=[1, 1, 1, 1])
-    h = tf.nn.batch_normalization(h, moving_mean, moving_variance, beta, gamma, BN_EPSILON)
+    with tf.variable_scope("resconv2"):
+        h = conv_block(h, bn=True, is_training_cond=is_training_cond)
 
     return inp + h
 
-def deconv_block(inp):
+def deconv_block(inp, relu=True, output_channels=64):
     """
-    TODO: Figure out what all these parameters do.
     TODO: Decide whether or not to add bias.
         biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
         deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
     """
-    k_h, k_w = 5, 5
-    d_h, d_w = 2, 2
-    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
-        initializer=tf.random_normal_initializer(stddev=0.02))
-    h = tf.nn.conv2d_transpose(inp, w, output_shape=output_shape, strides=[1, d_h, d_w, 1])
-    h = tf.nn.relu(h)
+    inp_shape = inp.get_shape()
+    kernel_shape = (3, 3, output_channels, inp_shape[-1])
+    output_shape = (int(inp_shape[0]), int(inp_shape[1] * 2), int(inp_shape[2] * 2), output_channels)
+    strides = [1, 2, 2, 1]
 
-    return h
-
-def conv_block(inp, relu=False, leaky_relu=False, bn=False):
     weights = tf.get_variable('weights', kernel_shape,
-        initializer=tf.random_normal_initializer())
-    h = tf.nn.conv2d(inp, weights, padding='SAME', strides=[1, 1, 1, 1])
+        initializer=tf.random_normal_initializer(stddev=0.02))
+    h = tf.nn.conv2d_transpose(inp, weights, output_shape, strides)
 
     if relu:
         h = tf.nn.relu(h)
 
+    return h
+
+def conv_block(inp, relu=False, leaky_relu=False, bn=False,
+               output_channels=64, stride=1, is_training_cond=None):
+    inp_shape = inp.get_shape()
+    kernel_shape = (3, 3, inp_shape[-1], output_channels)
+    strides = [1, stride, stride, 1]
+
+    weights = tf.get_variable('weights', kernel_shape,
+        initializer=tf.random_normal_initializer())
+    h = tf.nn.conv2d(inp, weights, strides, padding='SAME')
+
     if leaky_relu:
-        h = relu(h, alpha=0.1)
+        h = relu_block(h, alpha=0.1)
 
     if bn:
-        gamma = tf.get_variable('gamma', params_shape, initializer=tf.ones_initializer)
-        moving_mean = tf.get_variable('moving_mean', params_shape, initializer=tf.zeros_initializer)
-        moving_variance = tf.get_variable('moving_variance', params_shape, initializer=tf.ones_initializer)
-        h = tf.nn.batch_normalization(h, moving_mean, moving_variance, beta, gamma, BN_EPSILON)
+        pass # TODO Figure out how to do BN
+        # beta = tf.get_variable('beta', params_shape, 
+        #     initializer=tf.zeros_initializer)
+        # gamma = tf.get_variable('gamma', params_shape, 
+        #     initializer=tf.ones_initializer)
+        # moving_mean = tf.get_variable('moving_mean', params_shape, 
+        #     initializer=tf.zeros_initializer, trainable=False)
+        # moving_variance = tf.get_variable('moving_variance', params_shape, 
+        #     initializer=tf.ones_initializer, trainable=False)
+        # mean, variance = tf.nn.moments(h, [0, 1, 2])
+        # update_moving_mean = moving_averages.assign_moving_average(moving_mean,
+        #     mean, BN_DECAY)
+        # update_moving_variance = moving_averages.assign_moving_average(moving_variance,
+        #     variance, BN_DECAY)
+        # tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_mean)
+        # tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_variance)
+        # mean, variance = control_flow_ops.cond(is_training_cond, lambda: (mean, variance),
+        #     lambda: (moving_mean, moving_variance))
+        # h = tf.nn.batch_normalization(h, mean, variance, beta, gamma, BN_EPSILON)
+
+    if relu:
+        h = tf.nn.relu(h)
 
     return h
 
-def dense_block(inp, leaky_relu=False, sigmoid=False):
-    w = tf.get_variable("w", [shape[1], output_size], tf.random_normal_initializer(stddev=0.02))
+def dense_block(inp, leaky_relu=False, sigmoid=False,
+                output_size=1024):
+    h = tf.reshape(inp, [-1, 1])
+    inp_size = h.get_shape()[0]
 
-    b = tf.get_variable("b", [output_size], tf.constant_initializer(0.0))
+    w = tf.get_variable("w", [output_size, inp_size],
+        initializer=tf.random_normal_initializer(stddev=0.02))
+    b = tf.get_variable("b", [output_size],
+        initializer=tf.constant_initializer(0.0))
 
-    h = tf.matmul(inp, w) + b
+    h = tf.matmul(w, h) + b
+    h = tf.reshape(inp, [-1])
 
     if leaky_relu:
-        h = relu(h, alpha=0.1)
+        h = relu_block(h, alpha=0.1)
 
     if sigmoid:
         h = tf.nn.sigmoid(h)
@@ -120,16 +153,40 @@ def dense_block(inp, leaky_relu=False, sigmoid=False):
 
 class GAN(object):
     def __init__(self):
-        self.g_images = tf.placeholder(tf.float32, [self.batch_size, 96, 96, 3])
-        self.d_images = tf.placeholder(tf.float32, [self.batch_size, 784, 784, 3])
+        self.g_images = tf.placeholder(tf.float32, 
+            [BATCH_SIZE, LR_HEIGHT, LR_WIDTH, NUM_CHANNELS])
+        self.d_images = tf.placeholder(tf.float32,
+            [BATCH_SIZE, HR_HEIGHT, HR_WIDTH, NUM_CHANNELS])
+        self.is_training = tf.placeholder(tf.bool, [1])
 
     def build_model(self):
         with tf.variable_scope("G"):
             self.G = self.generator()
 
         with tf.variable_scope("D"):
-            self.D1 = self.discriminator(self.G)
-            self.D2 = self.discriminator(self.d_images)
+            self.D = self.discriminator(self.d_images)
+            tf.get_variable_scope().reuse_variables()
+            self.DG = self.discriminator(self.G)
+
+        # MSE Loss and Adversarial Loss for G
+        self.mse_loss = tf.reduce_mean(tf.squared_difference(self.d_images, self.G))
+        self.g_ad_loss = tf.reduce_mean(tf.neg(tf.log(self.DG)))
+
+        self.g_loss = self.mse_loss + self.g_ad_loss
+
+        # Real Loss and Adversarial Loss for D
+        self.d_loss_real = tf.reduce_mean(tf.neg(tf.log(self.D)))
+        self.d_loss_fake = tf.reduce_mean(tf.log(self.DG))
+
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+        t_vars = tf.trainable_variables()
+
+        self.d_vars = [var for var in t_vars if 'D/' in var.name]
+        self.g_vars = [var for var in t_vars if 'G/' in var.name]
+
+        # TODO Missing VGG loss and regularization loss. 
+        # Also missing weighting on losses.
 
     def generator(self):
         """Returns model generator, which is a DeConvNet.
@@ -143,22 +200,22 @@ class GAN(object):
             h = conv_block(self.g_images, relu=True)
 
         with tf.variable_scope("res1"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("res2"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("res3"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("res4"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("res5"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("res6"):
-            h = res_block(h)
+            h = res_block(h, self.is_training)
 
         with tf.variable_scope("deconv1"):
             h = deconv_block(h)
@@ -167,7 +224,7 @@ class GAN(object):
             h = deconv_block(h)
 
         with tf.variable_scope("conv2"):
-            h = conv_block(h)
+            h = conv_block(h, output_channels=3)
 
         return h
 
@@ -182,48 +239,66 @@ class GAN(object):
             h = conv_block(inp, leaky_relu=True)
 
         with tf.variable_scope("conv2"):
-            h = conv_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True, 
+                is_training_cond=self.is_training, stride=2)
 
         with tf.variable_scope("conv3"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True, 
+                is_training_cond=self.is_training, output_channels=128)
 
         with tf.variable_scope("conv4"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True,
+                is_training_cond=self.is_training, output_channels=128, stride=2)
 
         with tf.variable_scope("conv5"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True,
+                is_training_cond=self.is_training, output_channels=256, stride=1)
 
         with tf.variable_scope("conv6"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True,
+                is_training_cond=self.is_training, output_channels=256, stride=2)
 
         with tf.variable_scope("conv7"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True,
+                is_training_cond=self.is_training, output_channels=512, stride=1)
 
         with tf.variable_scope("conv8"):
-            h = res_block(h, leaky_relu=True, bn=True)
+            h = conv_block(h, leaky_relu=True, bn=True,
+                is_training_cond=self.is_training, output_channels=512, stride=2)
 
-        with tf.variable_scope("deconv2"):
-            h = deconv_block(h)
+        with tf.variable_scope("dense1"):
+            h = dense_block(h, leaky_relu=True, output_size=1024)
 
-        with tf.variable_scope("conv2"):
-            h = conv_block(h)
+        with tf.variable_scope("dense2"):
+            h = dense_block(h, sigmoid=True, output_size=1)
 
         return h
 
 class SuperRes(object):
     def __init__(self, sess, loader):
         self.sess = sess
+        self.loader = loader
         self.batch = loader.batch()
 
         self.GAN = GAN()
-        d_optim = tf.train.AdamOptimizer(self.learning_rate).minimize(self.GAN.d_loss)
-        g_optim = tf.train.AdamOptimizer(self.learning_rate).minimize(self.GAN.g_loss)
+        self.GAN.build_model()
+
+        d_optim = (tf.train.AdamOptimizer(LEARNING_RATE, beta1=BETA_1)
+            .minimize(self.GAN.d_loss, var_list=self.GAN.d_vars))
+        g_optim = (tf.train.AdamOptimizer(LEARNING_RATE, beta1=BETA_1)
+            .minimize(self.GAN.g_loss, var_list=self.GAN.g_vars))
 
     def train_model(self):
         tf.initialize_all_variables().run()
+        merged_summary_op = tf.merge_all_summaries()
 
+        summary_writer = tf.train.SummaryWriter(LOGS_DIR, graph_def=self.sess.graph_def)
+
+        # TODO Don't forget to pass in is_training bool!
+        # Also add: merged_summary_op
+        # summary_writer.add_summary(summary_str, iteration*total_batch + i)
         # Train
-        for epoch in range(num_epochs):
+        for epoch in range(NUM_EPOCHS):
             batch_xs, batch_ys = sess.run(self.batch)
             self.sess.run(self.d_optim, feed_dict={y: batch_ys})
             self.sess.run(self.g_optim, feed_dict={x: batch_xs})
@@ -234,8 +309,8 @@ class SuperRes(object):
 
 def main():
     sess = tf.Session()
-    file_list_2 = glob.glob("/etc/gan_images/high_res/*.png")
-    file_info_2 = (file_list_2, 768, 768)
+    file_list_2 = glob.glob(IMAGES)
+    file_info_2 = (file_list_2, HR_HEIGHT, HR_WIDTH)
     loader = Loader(file_info_2)
     model = SuperRes(sess, loader)
     model.train_model()
@@ -244,3 +319,4 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    main()
